bbade8e73e5c6dc0324216003427f3158677d7c6
Tim <tim@ninjatertle.com>
2018-12-30 18:52:05 +1100

    Moved Bool

---
+++ Entities/Editor/Drawers/BoolDrawer.cs
@@ -0,0 +1,25 @@
+﻿/*// <copyright file="BoolDrawer.cs" company="Timothy Raines">
+//     Copyright (c) Timothy Raines. All rights reserved.
+// </copyright>
+
+namespace BovineLabs.Editor.Drawers
+{
+    using BovineLabs.Common.Native;
+    using JetBrains.Annotations;
+    using Sirenix.OdinInspector.Editor;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Draws <see cref="Bool"/> in the inspector.
+    /// </summary>
+    [UsedImplicitly]
+    public class BoolDrawer : OdinValueDrawer<Bool>
+    {
+        /// <inheritdoc />
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            this.ValueEntry.SmartValue = EditorGUILayout.Toggle(label, this.ValueEntry.SmartValue);
+        }
+    }
+}*/
---
+++ Entities/Runtime/Helpers/Bool.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="Bool.cs" company="Timothy Raines">
+//     Copyright (c) Timothy Raines. All rights reserved.
+// </copyright>
+
+namespace BovineLabs.Entities.Helpers
+{
+    using System;
+    using Sirenix.OdinInspector;
+
+    /// <summary>
+    /// Burst currently does not support <see cref="bool"/> so this is a simple wrapper that acts like bool.
+    /// </summary>
+    [Serializable]
+    [InlineProperty]
+    public struct Bool : IEquatable<Bool>
+    {
+        /// <summary>
+        /// The value of the Bool. Should only be used by serializer.
+        /// </summary>
+        public byte Value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bool"/> struct.
+        /// </summary>
+        /// <param name="value">The default value.</param>
+        public Bool(bool value)
+        {
+            this.Value = value ? (byte)1 : (byte)0;
+        }
+
+        public static implicit operator Bool(bool b)
+        {
+            return new Bool(b);
+        }
+
+        public static implicit operator bool(Bool b)
+        {
+            return b.Value != 0;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(Bool other)
+        {
+            return this.Value == other.Value;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj is Bool b && this.Equals(b);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return (this.Value != 0).ToString();
+        }
+    }
+}
