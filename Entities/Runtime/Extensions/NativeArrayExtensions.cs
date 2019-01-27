2645f29ecf3b5dba853652c7a877b7ee76a8b78e
Tim <tim@ninjatertle.com>
2019-01-27 14:11:12 +1100

    Reinterpret for NativeArray.

---
+++ Entities/Runtime/Extensions/NativeArrayExtensions.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="NativeArrayExtensions.cs" company="Timothy Raines">
+//     Copyright (c) Timothy Raines. All rights reserved.
+// </copyright>
+
+namespace BovineLabs.Entities.Extensions
+{
+    using Unity.Collections;
+    using Unity.Collections.LowLevel.Unsafe;
+    using UnityEngine;
+
+    /// <summary>
+    /// The NativeArrayExtensions.
+    /// </summary>
+    public static class NativeArrayExtensions
+    {
+        public static unsafe NativeArray<TO> Reinterpret<TI, TO>(this NativeArray<TI> nativeArray)
+            where TI : struct
+            where TO : struct
+        {
+            var array = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<TO>(
+                nativeArray.GetUnsafePtr(),
+                nativeArray.Length,
+                Allocator.Invalid);
+
+            NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref array, AtomicSafetyHandle.Create());
+
+            return array;
+        }
+    }
+}
