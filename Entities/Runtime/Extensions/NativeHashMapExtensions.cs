6e31c99332f9f1b1b8339ad17ddb621ebfd757c6
Tim <tim@ninjatertle.com>
2019-02-23 13:20:09 +1100

    Removed legacy hash map extensions. Forgot they existed and have been replaced by official support.

--- Entities/Runtime/Extensions/NativeHashMapExtensions.cs
+++ Entities/Runtime/Extensions/NativeHashMapExtensions.cs
@@ -5,8 +5,6 @@
 namespace BovineLabs.Entities.Extensions
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using BovineLabs.Entities.Helpers;
     using Unity.Collections;
     using Unity.Collections.LowLevel.Unsafe;
@@ -30,91 +28,5 @@ namespace BovineLabs.Entities.Extensions
 #endif
             return NativeHashMapImposter<TKey, TValue>.TryReplaceValue(imposter.Buffer, key, item, false);
         }
-
-        /// <summary>
-        /// Get an Enumerator for a <see cref="NativeHashMap{TKey,TValue}"/>.
-        /// </summary>
-        /// <typeparam name="TKey">They key type of the hash map.</typeparam>
-        /// <typeparam name="TValue">the value type of the hash map.</typeparam>
-        /// <param name="hashMap">The hash map.</param>
-        /// <returns>The enumerator for the hash map.</returns>
-        public static NativeHashMapEnumerator<TKey, TValue> GetEnumerator<TKey, TValue>(
-            this NativeHashMap<TKey, TValue> hashMap)
-            where TKey : struct, IEquatable<TKey>
-            where TValue : struct
-        {
-            return new NativeHashMapEnumerator<TKey, TValue>(ref hashMap);
-        }
-
-        /// <summary>
-        /// A struct to handle enumerators of a <see cref="NativeHashMap{TKey,TValue}"/>.
-        /// </summary>
-        /// <typeparam name="TKey">The key.</typeparam>
-        /// <typeparam name="TValue">The value.</typeparam>
-        public unsafe struct NativeHashMapEnumerator<TKey, TValue> : IEnumerator<KeyValuePair<TKey, TValue>>
-            where TKey : struct, IEquatable<TKey>
-            where TValue : struct
-        {
-            private readonly NativeHashMapImposter<TKey, TValue> hashMap;
-            private readonly int* buckets;
-            private readonly byte* keys;
-            private readonly byte* values;
-
-            private int index;
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="NativeHashMapEnumerator{TKey, TValue}"/> struct.
-            /// </summary>
-            /// <param name="hashMap">The hash map to iterate.</param>
-            internal NativeHashMapEnumerator(ref NativeHashMap<TKey, TValue> hashMap)
-            {
-                // Convert to imposter so we can access internal fields
-                var imposter = (NativeHashMapImposter<TKey, TValue>)hashMap;
-
-                this.hashMap = hashMap;
-                this.index = -1;
-
-                this.buckets = (int*)imposter.Buffer->Buckets;
-                this.keys = imposter.Buffer->Keys;
-                this.values = imposter.Buffer->Values;
-            }
-
-            /// <inheritdoc />
-            public KeyValuePair<TKey, TValue> Current => new KeyValuePair<TKey, TValue>(
-                UnsafeUtility.ReadArrayElement<TKey>(this.keys, this.index),
-                UnsafeUtility.ReadArrayElement<TValue>(this.values, this.index));
-
-            /// <inheritdoc />
-            object IEnumerator.Current => this.Current;
-
-            /// <inheritdoc />
-            public bool MoveNext()
-            {
-                var length = this.hashMap.Buffer->BucketCapacityMask + 1;
-
-                for (this.index += 1; this.index < length; this.index++)
-                {
-                    if (this.buckets[this.index] != -1)
-                    {
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-            /// <inheritdoc />
-            public void Reset()
-            {
-                this.index = -1;
-            }
-
-            /// <inheritdoc />
-            public void Dispose()
-            {
-            }
-        }
-
-
     }
 }
--- Entities/Tests/Extensions/NativeHashMapExtensionsTests.cs
+++ Entities/Tests/Extensions/NativeHashMapExtensionsTests.cs
@@ -15,42 +15,6 @@ namespace BovineLabs.Entities.Tests.Extensions
     public class NativeHashMapExtensionsTest
     {
         [Test]
-        public void GetEnumerator()
-        {
-            const int elements = 64;
-
-            using (var map = new NativeHashMap<int, int>(elements, Allocator.TempJob))
-            {
-                var result = new Dictionary<int, int>();
-
-                for (var i = 0; i < elements; i++)
-                {
-                    map.TryAdd(i, i + 1);
-                }
-
-                // Quick remove test
-                map.Remove(0);
-
-                using (var enumerator = map.GetEnumerator())
-                {
-                    while (enumerator.MoveNext())
-                    {
-                        KeyValuePair<int, int> kvp = enumerator.Current;
-                        result.Add(kvp.Key, kvp.Value);
-                    }
-                }
-
-                Assert.AreEqual(elements - 1, result.Count);
-
-                for (var i = 1; i < elements; i++)
-                {
-                    Assert.IsTrue(result.TryGetValue(i, out var value));
-                    Assert.AreEqual(i + 1, value);
-                }
-            }
-        }
-
-        [Test]
         public void TryReplaceValue()
         {
             using (var map = new NativeHashMap<int, int>(4, Allocator.TempJob))
